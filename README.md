5349b28051b04d6edef536a5f61253a8a4600be3
Gungun974 <xfelix974@gmail.com>
2017-09-24 20:09:00 +0400

    test1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-"# jarvis-vera" 
+<!---
+IMPORTANT
+=========
+This README.md is displayed in the WebStore as well as within Jarvis app
+Please do not change the structure of this file
+Fill-in Description, Usage & Author sections
+Make sure to rename the [en] folder into the language code your plugin is written in (ex: fr, es, de, it...)
+For multi-language plugin:
+- clone the language directory and translate commands/functions.sh
+- optionally write the Description / Usage sections in several languages
+-->
+## Description
+Jarvis Vera est un plugin conçut pour s'interfacer avec la box vera
+
+## Usage
+```
+You: example of how to trigger the command
+Jarvis: output from Jarvis
+```
+
+## Author
+[Gunugn974](http://your.website)
---
+++ functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Here you can create functions which will be available from the commands file
+# You can also use here user variables defined in your config file
+# To avoid conflicts, name your function like this
+# pg_XX_myfunction () { }
+# pg for PluGin
+# XX is a short code for your plugin, ex: ww for Weather Wunderground
+# You can use translations provided in the language folders functions.sh
+
+pg_vc_turn () {
+    # $1: action [on|off]
+    # $2: received order
+    if [[ true ]]; then
+        say "$(pg_vc_lg "switching_$1" "$2")"
+        if [[ $1 == "on" ]]; then
+        	jv_curl $pg_vc_vera_ip+"http://192.168.1.14:3480/data_request?id=action&output_format=json&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=1&DeviceNum=40"
+        else
+        	jv_curl $pg_vc_vera_ip+"http://192.168.1.14:3480/data_request?id=action&output_format=json&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=0&DeviceNum=40"
+        fi
+        return $?
+    fi
+    say "$(pg_vc_lg "no_device_matching" "$2")"
+    return 0 # error already said above, no need to trigger error phrase
+}
